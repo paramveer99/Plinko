67267e216bbe6df3796d89e9b7e1397ddce756cf
paramveer99 <66951128+paramveer99@users.noreply.github.com>
2020-08-14 20:01:37 +0530

    Add files via upload

---
+++ Plinko/Plinko.js
@@ -0,0 +1,20 @@
+class Plinko{
+    constructor(x,y,r){
+        var options ={
+         isStatic: true   
+        }
+        this.body = Bodies.circle(x,y,10,options);
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push();
+        translate(pos.x,pos.y);
+        rotate(angle);
+        fill("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,10,10);
+        pop();
+    }
+}
---
+++ Plinko/sketch.js
@@ -0,0 +1,68 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var divisionHeight = 300;
+function setup() {
+  createCanvas(480,800);
+  engine = Engine.create();
+    world = engine.world;
+
+ ground = new Ground(250,790,600,20);
+  
+
+ for(var k = 0; k <=width; k = k + 80){
+   divisions.push(new Divisions(k, height-divisionHeight/2,10,divisionHeight));
+ }
+
+for (var j = 40; j <=width; j=j+50)
+{
+  plinkos.push(new Plinko(j,75));
+}
+for (var j = 15; j <=width-10; j=j+50)
+{
+  plinkos.push(new Plinko(j,175));
+}
+for (var j = 20; j <=width-10; j = j + 50)
+{
+  plinkos.push(new Plinko(j,275))
+}
+for (var j = 35; j <=width-10; j = j + 50)
+{
+  plinkos.push(new Plinko(j,375));
+}
+
+
+}
+
+function draw() {
+ background(0);  
+  Engine.update(engine);
+  ground.display();
+
+for(var j = 0; j < particles.length; j++){
+  particles[j].display();
+}
+
+for(var k = 0; k < divisions.length; k++){
+  divisions[k].display();
+}
+
+for(var i = 0; i < plinkos.length; i++){
+  plinkos[i].display();
+}
+
+if(frameCount%60===0){
+  particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+}
+
+
+
+  drawSprites();
+}
